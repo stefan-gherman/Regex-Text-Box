33728c6e013962fbcfc639e387e9db2ffacf2ecd
Stefan Gherman <scgheman@gmail.com>
2020-06-29 06:09:47 +0300

    Add project files.

---
+++ RegexTextBox/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexTextBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void NameCheck(string name)
+        {
+            string personName = "[a-zA-Z]+";
+            string whitespace = @"\s";
+            string pattern = "^(" + personName + whitespace + "*)+$";
+            if(!Regex.IsMatch(name, pattern))
+            {
+                MessageBox.Show("Name is invalid");
+            }
+            
+        }
+
+        public void EmailCheck(string email)
+        {
+            string emailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            if(!Regex.IsMatch(email, emailRegex ))
+            {
+                MessageBox.Show("Invalid Email");
+            }
+        }
+
+        private void svButton_Click(object sender, RoutedEventArgs e)
+        {
+            NameCheck(txtName.Text);
+            EmailCheck(txtEmail.Text);
+        }
+    }
+}
